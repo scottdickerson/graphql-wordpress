f81a1d932f3e9df13cec700a8495613e33513739
Scott Dickerson <6663002+scottdickerson@users.noreply.github.com>
2021-08-13 10:02:29 -0500

    feat(graphQL): first query to the graphQL endpoint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+// import gql from "fake-tag";
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
+
+export const SPACE_MISSION_URL = "https://api.spacex.land/graphql/";
+
+const client = new ApolloClient({
+  uri: SPACE_MISSION_URL,
+  cache: new InMemoryCache(),
+});
+
+const MISSION_QUERY = `
+{
+  launches {
+    mission_name
+  }
+}`;
+
+/**
+ * query response from the GraphQL Endpoint: https://api.spacex.land/graphql/
+ {
+  "data": {
+    "launches": [
+      {
+        "mission_name": "Thaicom 6"
+      },
+      {
+        "mission_name": "AsiaSat 6"
+      },
+*/
+
+// const client = ...
+
+// Start the network call
+client
+  .query({
+    query: gql`
+      ${MISSION_QUERY}
+    `,
+  })
+  .then(
+    // once the network call finishes, this callback is called
+    (result) => console.log(result) // callback function
+  );
+
+//This already runs the first
 function App() {
+  // move the client call here to force rerender of the app with the network response:
+  // www.apollographql.com/docs/react/get-started/ OR Use the traditional React way to call an backend API
+
   return (
     <div className="App">
       <header className="App-header">
